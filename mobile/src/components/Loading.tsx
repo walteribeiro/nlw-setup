c958c27191496fdad258576121c5bedab833b1aa
Walter Ribeiro <wribeiro@testreach.com>
2023-01-18 19:50:01 -0300

    Initial commit

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+
+export default function Loading() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#09090A'
+    }}>
+      <ActivityIndicator color="#7C3AED" size="large" />
+    </View>
+  );
+}
---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+import Habit from "./components/Habit"
+
+function App() {
+
+  return (
+    <Habit />
+  )
+}
+
+export default App
